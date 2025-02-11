eceadcf53601f5389690723dfe24ab12e0e2f1da
KELVIN_MUTUKU <mutukuk553@gmail.com>
2025-02-11 22:37:54 +0300

    Profie page updated to show borrowed books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_11_185922) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_11_192943) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_11_185922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "returned_at"
+    t.boolean "returned"
     t.index ["book_id"], name: "index_borrowings_on_book_id"
     t.index ["user_id"], name: "index_borrowings_on_user_id"
   end
